6a9de4f89cd9aa78d48f6fcfb829cc9cdb18ecda
MAHADI232 <rfat4322@gmail.com>
2024-01-03 21:54:53 +0600

    first commit

---
+++ exprot.js
@@ -0,0 +1,15 @@
+  let obj = {
+    name : 'hasan',
+    school : 'Raghunthpur high school',
+   
+ };
+
+ let jamil ={
+        fristName : 'kamal',
+        age : 123,
+        city : 'dhaka'
+    }
+ let arry =[12, 4, 54, 43, 345, 123, 1228]
+ export default{
+    obj, jamil,  arry 
+ };
